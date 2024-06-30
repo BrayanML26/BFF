74b9d215501fe86b22a9f77fb9136471e02ff23b
Brayan Martinez <brayan.26112001@gmail.com>
2024-06-30 14:51:53 -0400

    improve

--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksController } from './books.controller';
+import { BooksService } from './books.service';
+import { BooksModule } from './books.module';
 
 describe('BooksController', () => {
   let controller: BooksController;
@@ -7,6 +9,8 @@ describe('BooksController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BooksController],
+      providers: [BooksService], // Asegúrate de incluir BooksService aquí si no está en BooksModule.forRoot()
+      imports: [BooksModule], // Importa el módulo que contiene BooksController y BooksService
     }).compile();
 
     controller = module.get<BooksController>(BooksController);
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UsersModule } from './users.module';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -7,6 +9,8 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService], // Asegúrate de incluir UsersService aquí si no está en UsersModule.forRoot()
+      imports: [UsersModule], // Importa el módulo que contiene UsersController y UsersService
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
